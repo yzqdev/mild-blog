be5cb3130827742bbbe4d6c96cb411e29a7bad61
yzqdev <yzqdev@outlook.com>
2021-08-11 00:26:49 +0800

    添加路由

--- myblog-web/src/router.js
+++ myblog-web/src/router.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/view/Home.vue";
-import AdminMain from "@/view/admin/AdminMain.vue";
 import HomeMain from "@/view/home/HomeMain.vue";
 import HomeLink from "@/view/home/HomeLink.vue";
 import AdminPage from "@/view/AdminPage.vue";
@@ -8,6 +7,14 @@ import HomeTag from "@/view/home/HomeTag.vue";
 import HomeAbout from "@/view/home/HomeAbout.vue";
 import HomeBlog from "@/view/home/HomeBlog.vue";
 import AdminLogin from "@/view/admin/AdminLogin.vue";
+import AdminWelcome from "@/view/admin/AdminWelcome.vue";
+import AdminMain from "@/view/admin/AdminMain.vue";
+import ArticleList from "@/view/admin/ArticleList.vue";
+import CommentList from "@/view/admin/CommentList.vue";
+import CategoryList from "@/view/admin/CategoryList.vue";
+import TagList from "@/view/admin/TagList.vue";
+import LinkList from "@/view/admin/LinkList.vue";
+import SystemInfo from "@/view/admin/SystemInfo.vue";
 
 const routes = [
   {
@@ -29,7 +36,8 @@ const routes = [
         path: "tag/:id",
         name: "homeTag",
         component: HomeTag,
-      },  {
+      },
+      {
         path: "blog/:id",
         name: "homeBlog",
         component: HomeBlog,
@@ -42,16 +50,56 @@ const routes = [
     ],
   },
   { path: "/", redirect: "/home/main" },
-  { path: "/admin", redirect: "/admin/main" },
+  { path: "/admin", redirect: "/admin/login" },
   {
     path: "/admin",
     name: "admin",
     component: AdminPage,
     children: [
       {
-        path: "main",
+        path: "login",
         name: "adminLogin",
         component: AdminLogin,
+      },
+      {
+        path: "home",
+
+        component: AdminMain,
+        children: [
+          {
+            path: "welcome",
+            name: "adminWelcome",
+            component: AdminWelcome,
+          },
+          {
+            path: "article-list",
+            name: "articleList",
+            component: ArticleList,
+          },
+          {
+            path: "comment-list",
+            name: "commentList",
+            component: CommentList,
+          },
+          {
+            path: "category-list",
+            name: "categoryList",
+            component: CategoryList,
+          },
+          {
+            path: "tag-list",
+            name: "tagList",
+            component: TagList,
+          }, {
+            path: "link-list",
+            name: "linkList",
+            component: LinkList,
+          }, {
+            path: "system-info",
+            name: "systemInfo",
+            component: SystemInfo,
+          },
+        ],
       },
     ],
   },
