1df7643aacaf0dac3e2ae0bcf632840c0d5dd814
yzqdev <yzqdev@outlook.com>
2021-08-10 14:15:34 +0800

    添加前台json请求

--- myblog-server/src/main/java/com/site/blog/config/MybatisPlusConfig.java
+++ myblog-server/src/main/java/com/site/blog/config/MybatisPlusConfig.java
@@ -5,20 +5,16 @@ import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
- * @qq交流群 796794009
- * @qq 1320291471
- * @Description: mybatis-plus配置
- * @create 2020/12/27
+ * @author miemie
+ * @since 2018-08-10
  */
-@EnableTransactionManagement
 @Configuration
 public class MybatisPlusConfig {
 
     /**
-     * 分页插件
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题
      */
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
@@ -26,4 +22,9 @@ public class MybatisPlusConfig {
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
         return interceptor;
     }
+
+//    @Bean
+//    public ConfigurationCustomizer configurationCustomizer() {
+//        return configuration -> configuration.setUseDeprecatedExecutor(false);
+//    }
 }
--- myblog-web/src/router.js
+++ myblog-web/src/router.js
@@ -1,12 +1,35 @@
-
-import {
-  createRouter,
-  createWebHistory,
-} from "vue-router";
-
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/view/Home.vue";
+import Main from "@/view/Main.vue";
+import AdminPage from "@/view/AdminPage.vue";
 
 const routes = [
-
+  { path: "/", redirect: "/home/main" },
+  { path: "/admin", redirect: "/admin/main" },
+  {
+    path: "/home",
+    name: "home",
+    component: Home,
+    children: [
+      {
+        path: "main",
+        name: "main",
+        component: Main,
+      },
+    ],
+  },
+  {
+    path: "/admin",
+    name: "admin",
+    component: AdminPage,
+    children: [
+      {
+        path: "main",
+        name: "main",
+        component: Main,
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
