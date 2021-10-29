ba56ed0188bafdf231ce5b3aa0ede103e0f1f4d5
yzqdev <yzqdev@outlook.com>
2021-10-29 14:26:47 +0800

    add weblog

---
+++ myblog-server/src/main/java/com/site/blog/aop/WebLogAspect.java
@@ -0,0 +1,64 @@
+package com.site.blog.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+
+/**
+ * @author yanni
+ */
+@Aspect
+@Order(5)
+@Configuration
+public class WebLogAspect {
+
+    private ThreadLocal<Long> startTime = new ThreadLocal<>();
+
+    //使用@Pointcut定义一个切入点，可以是一个规则表达式，比如下例中某个package下的所有函数，也可以是一个注解等。
+    @Pointcut("execution(public * com.example.aop.controller.*.*(..))")
+    public void webLog() {
+    }
+
+    private Logger log = LoggerFactory.getLogger(this.getClass());
+
+    //使用@Before在切入点开始处切入内容
+    @Before("webLog()")
+    public void doBefore(JoinPoint joinPoint) throws Throwable {
+        startTime.set(System.currentTimeMillis());
+
+        // 接收到请求，记录请求内容
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+        // 记录下请求内容
+        log.info("请求URL : " + request.getRequestURL().toString());
+        log.info("请求HTTP_METHOD : " + request.getMethod());
+        log.info("请求IP : " + request.getRemoteAddr());
+        log.info("请求CLASS_METHOD : " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        log.info("请求参数值 : " + Arrays.toString(joinPoint.getArgs()));
+
+    }
+
+
+    //使用@AfterReturning在切入点return内容之后切入内容（可以用来对处理返回值做一些加工处理）
+    @AfterReturning(returning = "ret", pointcut = "webLog()")
+    public void doAfterReturning(Object ret) throws Throwable {
+        // 处理完请求，返回内容
+        log.info("响应RESPONSE : " + ret);
+        log.info("响应时间SPEND TIME : " + (System.currentTimeMillis() - startTime.get()));
+    }
+
+}
