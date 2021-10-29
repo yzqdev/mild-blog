90b18f10e6ded3bab04f4c0c89defe6f2b50f090
yzqdev <yzqdev@outlook.com>
2021-10-29 14:27:25 +0800

    update configs

---
+++ myblog-server/src/main/java/com/site/blog/listener/AppListener.java
@@ -0,0 +1,29 @@
+package com.site.blog.listener;
+
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author yanni
+ * @date time 2021/10/29 12:13
+ * @modified By:
+ */
+@Configuration
+public class AppListener implements ApplicationListener<WebServerInitializedEvent> {
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent event) {
+        int port = event.getApplicationContext().getWebServer().getPort();
+        InetAddress address = null;
+        try {
+            address = InetAddress.getLocalHost();
+            System.out.println("http://" + address.getHostAddress() + ":" + port + "/swagger-ui/");
+            System.out.println("http://" + address.getHostAddress() + ":" + port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
