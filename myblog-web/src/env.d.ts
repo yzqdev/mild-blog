0a7dcd94f6991ec753e3c21d1448f262102eeb6c
yzqdev <yzqdev@outlook.com>
2022-05-14 22:31:43 +0800

    use vue reactive transform

---
+++ myblog-web/src/env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vue/macros-global" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: string;
+    VUE_ROUTER_MODE: 'hash' | 'history' | 'abstract' | undefined;
+    VUE_ROUTER_BASE: string | undefined;
+  }
+}
