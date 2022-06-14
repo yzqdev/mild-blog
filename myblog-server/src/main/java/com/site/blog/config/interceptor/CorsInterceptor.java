c42a6fc771e767a4ecbea9a0111eb2df295746f0
yzqdev <yzqdev@outlook.com>
2022-06-15 00:19:09 +0800

    update cors

--- myblog-server/src/main/java/com/site/blog/config/interceptor/CorsInterceptor.java
+++ myblog-server/src/main/java/com/site/blog/config/interceptor/CorsInterceptor.java
@@ -1,5 +1,7 @@
 package com.site.blog.config.interceptor;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,16 +16,23 @@ import javax.servlet.http.HttpServletResponse;
  * @Modified By:
  */
 @Component
+@Slf4j
 public class CorsInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin") );
+        log.info(request.getHeader("origin"));
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
         //response.setHeader("Access-Control-Allow-Origin", "http://192.168.72.132:2801" );
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, token, Content-Type, Accept");
+                "Origin, X-Requested-With, token, Content-Type, Accept, version");
+        //浏览器会先发送一个试探请求OPTIONS,然后才会发送真正的请求，为了避免拦截器拦截两次请求，所以不能让OPTIONS请求通过
+        if (request.getMethod().equals(HttpMethod.OPTIONS.name())){
+            response.setStatus(HttpServletResponse.SC_OK);
+            return false;
+        }
         return true;
     }
 
