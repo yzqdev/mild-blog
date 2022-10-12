63fb058d8f07e02e83f7dfcb238769e6f8efae0c
yzqdev <yzqdev@outlook.com>
2022-10-12 13:31:47 +0800

    update deps

--- myblog-kt/build.gradle.kts
+++ myblog-kt/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.7.3"
-    id("io.spring.dependency-management") version "1.0.13.RELEASE"
-    kotlin("jvm") version "1.7.10"
-    kotlin("plugin.spring") version "1.7.10"
+    id("org.springframework.boot") version "2.7.4"
+    id("io.spring.dependency-management") version "1.0.14.RELEASE"
+    kotlin("jvm") version "1.7.20"
+    kotlin("plugin.spring") version "1.7.20"
 }
 
 group = "com.site.blog"
@@ -35,15 +35,15 @@ dependencies {
     implementation("io.pebbletemplates:pebble-spring-boot-starter:3.1.5")
     implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
     // https://mvnrepository.com/artifact/cn.hutool/hutool-all
-    implementation("cn.hutool:hutool-all:5.8.5")
+    implementation("cn.hutool:hutool-all:5.8.8")
 // https://mvnrepository.com/artifact/com.alibaba/druid-spring-boot-starter
     implementation("com.alibaba:druid-spring-boot-starter:1.2.11")
 // https://mvnrepository.com/artifact/net.coobird/thumbnailator
     implementation("net.coobird:thumbnailator:0.4.17")
 // https://mvnrepository.com/artifact/org.apache.commons/commons-text
-    implementation("org.apache.commons:commons-text:1.9")
+    implementation("org.apache.commons:commons-text:1.10.0")
 // https://mvnrepository.com/artifact/com.freewayso/image-combiner
-    implementation("com.freewayso:image-combiner:2.3.5")
+    implementation("com.freewayso:image-combiner:2.3.7")
 // https://mvnrepository.com/artifact/com.dtflys.forest/forest-spring-boot-starter
     implementation("com.dtflys.forest:forest-spring-boot-starter:1.5.26")
 
@@ -54,6 +54,9 @@ dependencies {
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+repositories {
+    mavenCentral()
 }
 
 tasks.withType<KotlinCompile> {
